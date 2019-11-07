66b13bc4f11ab65c1ee419b4429f60f79bea6e09
maelle.berthier <maelle.berthier@centrale-marseille.fr>
2019-11-07 14:35:53 +0100

    professor

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 
 def create_app():
     app = Flask(__name__)
+    Bootstrap(app)
 
     @app.route('/')
     def index():
         return '<h1>ECM Bonjour</h1>'
 
-
     @app.route('/user/<name>')
     def user(name):
         return render_template('user.html', name=name)
+
+    @app.route('/professor')
+    def my_api_route():
+        return {
+            "name": "Adrien",
+            "birthday": "02 January",
+            "age": 85,
+            "sex": None,
+            "friends": ["Amadou", "Mariam"]
+        }
 
     return app
--- conftest.py
+++ conftest.py
@@ -1,6 +1,5 @@
 import pytest
-
-
+from flask import template_rendered
 
 from app import create_app
 
@@ -13,3 +12,17 @@ def user_name():
 def app():
     app = create_app()
     return app
+
+
+@pytest.fixture
+def captured_templates(app):
+    recorded = []
+
+    def record(sender, template, context, **extra):
+        recorded.append((template, context))
+
+    template_rendered.connect(record, app)
+    try:
+        yield recorded
+    finally:
+        template_rendered.disconnect(record, app)
